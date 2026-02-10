197793e2b64e38766e1d5a4f4f066a52f9e8e406
Abdul Sattar <info.abdulsattardeveloper@gmail.com>
2026-02-10 20:53:41 +0500

    admin-Setting

--- admin/admin-settings.php
+++ admin/admin-settings.php
@@ -1,11 +1,7 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
-/*
- |----------------------------------
- | Admin Menu
- |----------------------------------
- */
+/* Admin Menu */
 add_action('admin_menu', function () {
   add_options_page(
     'Floating Contact Form',
@@ -16,40 +12,27 @@ add_action('admin_menu', function () {
   );
 });
 
-/*
- |----------------------------------
- | Register Settings
- |----------------------------------
- */
+/* Register Settings */
 add_action('admin_init', function () {
-
   register_setting('faic_settings_group', 'faic_theme_color');
   register_setting('faic_settings_group', 'faic_header_text');
   register_setting('faic_settings_group', 'faic_submit_text');
   register_setting('faic_settings_group', 'faic_online_dot');
 });
 
-/*
- |----------------------------------
- | Settings Page HTML
- |----------------------------------
- */
+/* Settings Page HTML */
 function faic_settings_page()
-{
-?>
+{ ?>
   <div class="wrap">
     <h1>Floating Contact Form Settings</h1>
-
     <form method="post" action="options.php">
       <?php settings_fields('faic_settings_group'); ?>
-
       <table class="form-table">
 
         <tr>
           <th scope="row">Main Theme Color</th>
           <td>
-            <input type="color"
-              name="faic_theme_color"
+            <input type="color" name="faic_theme_color"
               value="<?php echo esc_attr(get_option('faic_theme_color', '#d10000')); ?>">
           </td>
         </tr>
@@ -57,9 +40,7 @@ function faic_settings_page()
         <tr>
           <th scope="row">Header Text</th>
           <td>
-            <input type="text"
-              class="regular-text"
-              name="faic_header_text"
+            <input type="text" class="regular-text" name="faic_header_text"
               value="<?php echo esc_attr(get_option('faic_header_text', 'Get In Touch')); ?>">
           </td>
         </tr>
@@ -67,9 +48,7 @@ function faic_settings_page()
         <tr>
           <th scope="row">Submit Button Text</th>
           <td>
-            <input type="text"
-              class="regular-text"
-              name="faic_submit_text"
+            <input type="text" class="regular-text" name="faic_submit_text"
               value="<?php echo esc_attr(get_option('faic_submit_text', 'Submit')); ?>">
           </td>
         </tr>
@@ -78,9 +57,7 @@ function faic_settings_page()
           <th scope="row">Show Online Dot</th>
           <td>
             <label>
-              <input type="checkbox"
-                name="faic_online_dot"
-                value="1"
+              <input type="checkbox" name="faic_online_dot" value="1"
                 <?php checked(get_option('faic_online_dot', 1), 1); ?>>
               Enable green online dot
             </label>
@@ -88,9 +65,7 @@ function faic_settings_page()
         </tr>
 
       </table>
-
       <?php submit_button('Save Settings'); ?>
     </form>
   </div>
-<?php
-}
+<?php }
